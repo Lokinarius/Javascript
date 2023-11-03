7ef470def49accf85d36c49ba78f54325b1fc994
William Galvão <Liam_draven@outlook.com>
2023-11-03 19:31:47 -0300

    Variáveis e tipos primitivos

---
+++ Módulo B/Variáveis e tipos primitivos/002.js
@@ -0,0 +1,23 @@
+//espaços são divididos pelas variáveis
+//toda variável possui um valor numérico
+//string é uma cadeia de caracteres 
+//ex:
+//VAR s1 = "Javascript"     || n1[5]   s1[Javascript]
+//VAR s2 = 'Curso em Vídeo' || n2[8.5] s2[Curso em vídeo]
+//VAR s3 = `Guanabara`      || n3[15]  s3[Guanabara]
+// var --> variável
+// "=" --> recebe
+//typeof --> achar o tipo no terminal
+
+
+//                                  Tipos primitivos
+//typeof --> achar o tipo no terminal
+//number: "5, 18, -12, 6.8, -0.5, 9.0"
+    //NaN
+    //infinity
+//string: "Javascript, William, Churrasco"
+//boolean: "true, false"
+//null: valor nulo
+//object:
+    //Array
+//function
