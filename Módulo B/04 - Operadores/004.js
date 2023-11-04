5c6c3aedd1179cf215e369e029b853bd11dc97f2
William Galvão <Liam_draven@outlook.com>
2023-11-04 20:02:09 -0300

    Operadores

---
+++ Módulo B/04 - Operadores/004.js
@@ -0,0 +1,53 @@
+//                      Operadores
+//aritiméticos
+    // +  soma
+    // -  subtração
+    // *  multiplicação
+    // /  divisão
+    // %  resto
+    // ** potência
+var n1 = 5 + 3              // n1 = 8
+var n2 = n1 % 5             // n2 = 3
+var n3 = 5 * n2 ** 2        // n3 = 45
+var n4 = 10 - n1 / 2        // n4 = 6
+var n5 = 6 * 2 / n4         // n5 = 2
+var n6 = n2 % n5 + 4 / n5   // n6 = 3
+//atribuição
+    // var n = 1
+    // n = n + 4  --> n += 4 --> n++
+    // n = n - 5  --> n -= 5 --> n--
+    // n = n * 4  --> n *= 4
+    // n = n / 2  --> n /= 2
+    // n = n ** 2 --> n **= 2
+    // n = n % 5  --> n %= 5
+//relacionais
+    // > Maior que
+    // < Menor que
+    // >= Maior ou igual a
+    // <= Menor ou igual a
+    // == Igual a
+    // != Diferente de
+    // === Identico a
+    // !== Desigual a
+//lógicos
+    // !  Negação (não)
+        // !true  --> false
+        // !false --> true
+    // && Conjunção (e)
+        // true && true   --> true
+        // true && false  --> false
+        // false && true  --> false
+        // false && false --> false
+        //o resultado é verdadeiro apenas quandos os dois são verdadeiros
+    // || Disjunção (ou)
+        // true || true   --> true
+        // true || false  --> true
+        // false || true  --> true
+        // false || false --> false
+        //basta que apenas um seja verdadeiro para que o resultado seja verdadeiro
+//ternário
+    // ?: --> teste ? true : false
+    //o operador ternário é um operador lógico
+     //Exemplo:
+        // média >= 7.0 ? "Aprovado" : "Reprovado"
+        //
