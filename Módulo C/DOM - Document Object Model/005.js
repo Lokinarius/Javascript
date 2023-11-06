6b07a0441dfbce3bcfda0f920b2453a737b9ac0f
William Galvão <Liam_draven@outlook.com>
2023-11-06 16:34:28 -0300

    DOM

---
+++ Módulo C/DOM - Document Object Model/005.js
@@ -0,0 +1,13 @@
+//window.document.getElementsByTagNameNS
+var p1 = window.document.getElementsByTagName('p')[0]
+window.alert(p1.innerText)
+//Por Marca
+var m = window.document.getElementById('msg');
+m.style.color = 'lightgreen';
+//Por ID
+//window.document.getElementsByName
+//Por Nome
+//window.document.getElementsByClassName
+//Por Classe
+//var m = window.document.querySelector('div.msg')
+//m.style.background = 'gray'
