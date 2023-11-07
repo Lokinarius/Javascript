66636ccc746f68f1d16e3e3ed7ff7451f243fc9d
William Galvão <Liam_draven@outlook.com>
2023-11-07 17:13:46 -0300

    008

--- Módulo D/01 - Condições Simples/008.js
+++ Módulo D/01 - Condições Simples/008.js
@@ -7,14 +7,3 @@ function calcular(){
         res.innerHTML += ' Você foi <strong>MULTADO</strong> por excesso de velocidade'
     }
 }
-
-
-
-
-
-//console.log('Olá, mundo!')
-//var vel = 60.5
-//console.log(`A velocidade do seu carro é ${vel}km/h`)
-//if(vel > 60){
-    //console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-//}
