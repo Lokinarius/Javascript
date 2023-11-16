62b8a39f7cb102fb23f4fdb71938258d9845ebbb
William Galvão <Liam_draven@outlook.com>
2023-11-16 18:32:27 -0300

    Repetições 1

---
+++ Módulo E/Repetições/013.js
@@ -0,0 +1,13 @@
+//estrutura de repetição no inicio
+var c = 1
+while(c > 0 && c<= 10) {
+    console.log(`Me fodi ${c} vezes`)
+    c++
+}
+
+//estrutura de repetição no final
+var c = 10
+ do{
+    console.log(`Vou me churrascar em ${c} dias`)
+    c--
+}while(c >= 1)
